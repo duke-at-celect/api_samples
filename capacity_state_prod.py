56afaa510d9e48bdfbb7de1763d896f6594082d3
duke-at-celect <33836287+duke-at-celect@users.noreply.github.com>
2017-11-21 15:35:28 -0500

    Update capacity_state_prod.py
    Added additional logging in the CSV for 'store_type' and 'enable_ship_from_store'.
    
    Also, if not state is provided during a get operation, the script will throw an error rather than default to US.

--- capacity_state_prod.py
+++ capacity_state_prod.py
@@ -15,49 +15,50 @@ api_key = '<YOUR_API_KEY>'
 def get_state(brand):
 	file = region + '_capacity_state.csv'
 	payload = {
-		'client_id': client_id,
-		'api_key': api_key,
-		'data': {'brand': brand}}
+	'client_id': client_id, 
+	'api_key': api_key,
+	'data': {'brand': brand}}
 	head = {
-		'X-Http-Method-Override': 'GET',
-		'Content-Type': 'application/json'
+	'X-Http-Method-Override': 'GET',
+	'Content-Type': 'application/json'
 	}
 	r = requests.post(get_url, headers=head, data=json.dumps(payload))
 	# print(r.status_code)  # debug
 	# print(r.text)  # debug
-	data = json.loads(r.text)
-	d2 = json_normalize(data['results'])  # returns df
-	cap_state = d2[['country', 'store_id', 'shipments_capacity']]
+	data=json.loads(r.text)
+	d2= json_normalize(data['results'])  # returns df
+	cap_state = d2[['country', 'store_id', 'shipments_capacity', 'store_type', 'enable_ship_from_store']]
 	print('Store Count: ', len(cap_state.index))
 	# print(cap_state.head(5))
 	cap_state.to_csv(file)
-
+	
 
 def write_state():
 	count = 0
 	rawData = file
 	df = pd.read_csv(rawData)
+	df[['store_id', 'shipments_capacity']]
 	df['store_id'] = df['store_id'].astype(str)
 	# SET BRAND BASED ON 'country'
-	if df.loc[0, 'country'] == 'US':
+	if df.loc[0,'country'] == 'US':
 		brand = '01'
 	else:
-		brand = '02 '
+		brand = '02'
 	# READ store_id and shipments_capacity into dictionary
-	df = df[['store_id', 'shipments_capacity']]
+	df=df[['store_id', 'shipments_capacity']]
 	cap_dict = dict(zip(df.store_id, df.shipments_capacity))
 	# print(cap_dict)  # debug
 	for key, value in cap_dict.items():
 		# print(key, value)
 		x_id = key
 		url2 = patch_url + x_id
-		# print(url2)  # debug
+		print(url2)  # debug
 		# request goes here
 		payload = {
-			'client_id': client_id,
-			'api_key': api_key,
-			'data': {
-				'store': {
+		'client_id': client_id, 
+		'api_key': api_key,
+		'data': {
+			'store':{
 			'shipmentsCapacity': value
 			},
 			'brand': brand}}
@@ -66,8 +67,8 @@ def write_state():
 		}
 		# print(payload)  # debug
 		r = requests.patch(url2, headers=head, data=json.dumps(payload))
-		print(r.text)
-		count += 1
+		# print(r.text)
+		count +=1
 		# print(count)
 		if r.status_code > 200:
 			print('ERROR', r.status_code)
@@ -77,28 +78,26 @@ def write_state():
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--state', choices=['get', 'set'],
-	help='Either get current state or set state to a previous get.')
-parser.add_argument('--region',  choices=['ca', 'us'],
-	help="Required with --state get Must be either 'ca' for Canada or 'us' for United States")
-parser.add_argument("--file",
-	help="Required with --state set. The file to process - include path if necessary")
+parser.add_argument('--state', choices=['get', 'set'], help='Either get current state or set state to a previous get.')
+parser.add_argument('--region',  choices=['ca', 'us'], help="Required with --state get Must be either 'ca' for Canada or 'us' for United States")
+parser.add_argument("--file",  help="Required with --state set. The file to process - include path if necessary")
 args = parser.parse_args()
 # region = args.region
 file = args.file
 state = args.state
 region = args.region
-
 if state == 'get':
 	if region == 'ca':
 		print('Getting current state for region ', region)
 		brand = '02'
 		get_state(brand)
-	else:
-		region = 'us'
+	elif region == 'us':
 		print('Getting current state for region ', region)
 		brand = '01'
 		get_state(brand)
+	else:
+		print('ERROR : missing --region : use --help for more information : Exiting')
+		exit()
 elif state == 'set':
 	if file.endswith('.csv'):
 		print("Processing file...")
